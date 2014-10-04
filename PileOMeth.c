e45a2959296ad38991bc702fe391fc5aa4797651
dpryan79 <dpryan@dpryan.com>
2014-10-04 18:53:21 +0200

    Initial commit. It works, but it's not terribly pretty.

---
+++ PileOMeth.c
@@ -0,0 +1,317 @@
+#include "htslib/sam.h"
+#include "htslib/hts.h"
+#include "htslib/faidx.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+/****************************************
+*
+* TO DO
+*
+* Add an -l option to just parse a specific region
+* add an -r/--region REGION option to just look at one region
+* Enable ignoring discordant and/or singleton alignments
+* Enable trimming based on M-bias
+* Change the default max depth to 1000 and allow the user to further change it
+* Support CRAM files
+* Is it possible to support non-directional libraries with this method?
+*    config.bai = bam_index_load(argv[optind+1]); //How do we close this?
+* 
+****************************************/
+
+inline int isCpG(char *seq, int pos, int seqlen) {
+    if(pos+1 == seqlen) return 0;
+    if(*(seq+pos) == 'C' || *(seq+pos) == 'c') {
+        if(*(seq+pos+1) == 'G' || *(seq+pos+1) == 'G') return 1;
+        return 0;
+    } else if(*(seq+pos) == 'G' || *(seq+pos) == 'g') {
+        if(pos == 0) return 0;
+        if(*(seq+pos-1) == 'C' || *(seq+pos-1) == 'c') return -1;
+        return 0;
+    }
+    return 0;
+}
+
+inline int isCHG(char *seq, int pos, int seqlen) {
+    if(pos+2 >= seqlen) return 0;
+    if(*(seq+pos) == 'C' || *(seq+pos) == 'c') {
+        if(*(seq+pos+2) == 'G' || *(seq+pos+2) == 'G') return 1;
+        return 0;
+    } else if(*(seq+pos) == 'G' || *(seq+pos) == 'g') {
+        if(pos <= 1) return 0;
+        if(*(seq+pos-2) == 'C' || *(seq+pos-2) == 'c') return -1;
+        return 0;
+    }
+    return 0;
+}
+
+inline int isCHH(char *seq, int pos, int seqlen) {
+    if(pos+2 >= seqlen) return 0;
+    if(*(seq+pos) == 'C' || *(seq+pos) == 'c') return 1;
+    else if(*(seq+pos) == 'G' || *(seq+pos) == 'g') {
+        if(pos <= 1) return 0;
+        return -1;
+    }
+    return 0;
+}
+
+typedef struct {
+    int KeepCpG, KeepCHG, KeepCHH;
+    int minMapq, minPhred;
+    FILE **output_fp;
+    htsFile *fp;
+    hts_idx_t *bai;
+    faidx_t *fai;
+} Config;
+
+typedef struct {
+    Config *config;
+    bam_hdr_t *hdr;
+} mplp_data;
+
+int getStrand(bam1_t *b) {
+    if(b->core.flag & BAM_FPAIRED) {
+        if((b->core.flag & 0x50) == 0x50) return 1; //Read1, reverse comp. == OB
+        else if(b->core.flag & 0x40) return 0; //Read1, forward == OT
+        else if((b->core.flag & 0x90) == 0x90) return 0; //Read2, reverse comp. == OT
+        else if(b->core.flag & 0x80) return 0; //Read2, forward == OB
+        return -1; //One of the above should be set!
+    } else {
+        if(b->core.flag & 0x10) return 1; //Reverse comp. == OB
+        return 0;
+    }
+}
+
+int updateMetrics(Config *config, const bam_pileup1_t *plp) {
+    uint8_t base = bam_seqi(bam_get_seq(plp->b), plp->qpos);
+    int strand = getStrand(plp->b); //0=OT, 1=OB, 2=CTOT, 3=CTOB
+
+    //Is the phred score even high enough?
+    if(bam_get_qual(plp->b)[plp->qpos] < config->minMapq) return 0;
+
+    if(base == 2 && strand == 0) return 1; //C on an OT read
+    else if(base == 8 && strand == 0)  return -1; //T on an OT read
+    else if(base == 4 && strand == 1) return 1; //G on an OB read
+    else if(base == 1 && strand == 1) return -1; //A on an OB read
+    return 0;
+}
+
+//This needs to be defined
+int filter_func(void *data, bam1_t *b) {
+    int rv;
+    mplp_data *ldata = (mplp_data *) data;
+
+    while(1) {
+        rv = bam_read1(ldata->config->fp->fp.bgzf, b);
+        if(rv<0) return rv;
+        if(b->core.tid == -1 || b->core.flag & BAM_FUNMAP) continue;
+        if(b->core.qual < ldata->config->minMapq) continue;
+        break;
+    }
+    return rv;
+}
+
+void extractCalls(Config *config) {
+    bam_hdr_t *hdr = bam_hdr_read(config->fp->fp.bgzf);
+    bam_mplp_t iter;
+    int ret, tid, pos, n_plp, i, seqlen, type, rv;
+    int ctid = -1; //The tid of the contig whose sequence is stored in "seq"
+    uint32_t nmethyl, nunmethyl;
+    const bam_pileup1_t **plp = calloc(1, sizeof(bam_pileup1_t *));
+    char *seq = NULL;
+    mplp_data *data = malloc(sizeof(mplp_data));
+
+    data->config = config;
+    data->hdr = hdr;
+
+    //Start the pileup
+    iter = bam_mplp_init(1, filter_func, (void **) &data);
+    bam_mplp_init_overlaps(iter);
+    bam_mplp_set_maxcnt(iter, 2000); //Should be user definable
+    while((ret = bam_mplp_auto(iter, &tid, &pos, &n_plp, plp)) > 0) {
+        //Do we need to process this position?
+        if(tid != ctid) {
+            if(seq != NULL) free(seq);
+            seq = faidx_fetch_seq(config->fai, hdr->target_name[tid], 0, faidx_seq_len(config->fai, hdr->target_name[tid]), &seqlen);
+            if(seqlen < 0) {
+                fprintf(stderr, "faidx_fetch_seq returned %i while trying to fetch the sequence for tid %i (%s)!\n",\
+                    seqlen, tid, hdr->target_name[tid]);
+                fprintf(stderr, "Note that the output will be truncated!\n");
+                return;
+            }
+            ctid = tid;
+        }
+        if(config->KeepCpG && isCpG(seq, pos, seqlen)) {
+            type = 0;
+        } else if(config->KeepCHG && isCHG(seq, pos, seqlen)) {
+            type = 1;
+        } else if(config->KeepCHH && isCHH(seq, pos, seqlen)) {
+            type = 2;
+        } else {
+            continue;
+        }
+
+        nmethyl = nunmethyl = 0;
+        for(i=0; i<n_plp; i++) {
+            rv = updateMetrics(config, plp[0]+i);
+            if(rv > 0) nmethyl++;
+            else if(rv<0) nunmethyl++;
+        }
+
+        if(nmethyl+nunmethyl) fprintf(config->output_fp[type], "%s\t%i\t%i\t%f\t%" PRIu32 "\t%" PRIu32 "\n", \
+            hdr->target_name[tid], pos, pos+1, 1000.0 * ((double) nmethyl)/(nmethyl+nunmethyl), nmethyl, nunmethyl);
+    }
+
+    bam_hdr_destroy(hdr);
+    bam_mplp_destroy(iter);
+    free(data);
+    free(plp);
+    if(seq != NULL) free(seq);
+}
+
+void usage(char *prog) {
+    fprintf(stderr, "\nUsage: %s [OPTIONS] reference.fa file.bam\n", prog);
+    fprintf(stderr,
+"\n"
+"Options:\n"
+" -q INT           Minimum MAPQ threshold to include an alignment (default 5)\n"
+" -p INT           Minimum Phred threshold to include a base (default 10)\n"
+" -o, --opref STR  Output filename prefix. CpG/CHG/CHH metrics will be\n"
+"                  output to STR_CpG.bedGraph and so on.\n"
+" --noCpG          Do not output CpG methylation metrics\n"
+" --CHG            Output CHG methylation metrics\n"
+" --CHH            Output CHH methylation metrics\n");
+}
+
+int main(int argc, char *argv[]) {
+    char *opref = NULL, *oname, *p;
+    int c;
+    Config config;
+
+    //Defaults
+    config.KeepCpG = 1; config.KeepCHG = 0; config.KeepCHH = 0;
+    config.minMapq = 5; config.minPhred = 5;
+    config.fai = NULL;
+    config.fp = NULL;
+    config.bai = NULL;
+
+    static struct option lopts[] = {
+        {"opref", 1, NULL,              'o'},
+        {"noCpG", 0, NULL,                1},
+        {"CHG",   0, NULL,                2},
+        {"CHH",   0, NULL,                3},
+        {"help",  0, NULL,              'h'}
+    };
+    while((c = getopt_long(argc, argv, "q:p:", lopts,NULL)) >= 0) {
+        switch(c) {
+        case 'h' :
+            usage(argv[0]);
+            return 0;
+        case 'o' :
+            opref = strdup(optarg);
+            break;
+        case 1 :
+            config.KeepCpG = 0;
+            break;
+        case 2 :
+            config.KeepCHG = 1;
+            break;
+        case 3 :
+            config.KeepCHH = 1;
+            break;
+        case 'q' :
+            config.minMapq = atoi(optarg);
+            break;
+        case 'p' :
+            config.minPhred = atoi(optarg);
+            break;
+        default :
+            fprintf(stderr, "Invalid option '%c'\n", c);
+            usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if(argc == 1) {
+        usage(argv[0]);
+        return 0;
+    }
+    if(argc-optind != 2) {
+        fprintf(stderr, "You must supply a reference genome in fasta format and an input BAM file!!!\n");
+        usage(argv[0]);
+        return -1;
+    }
+
+    //Open the files
+    config.fai = fai_load(argv[optind]);
+    if(config.fai == NULL) {
+        fprintf(stderr, "Couldn't open the index for %s!\n", argv[optind]);
+        usage(argv[0]);
+        return -2;
+    }
+    config.fp = hts_open(argv[optind+1], "rb"); //Does this return NULL on error?
+    if(config.fp == NULL) {
+        fprintf(stderr, "Couldn't open %s for reading!\n", argv[optind+1]);
+        return -4;
+    }
+    config.bai = sam_index_load(config.fp, argv[optind+1]); //How do we close this?
+    if(config.bai == NULL) {
+        fprintf(stderr, "Couldn't open the index for %s!\n", argv[optind+1]);
+        return -5;
+    }
+
+    //Output files
+    config.output_fp = malloc(sizeof(FILE *) * 3);
+    if(opref == NULL) {
+        opref = strdup(argv[optind+1]);
+        p = strrchr(opref, '.');
+        *p = '\0';
+    }
+    oname = malloc(sizeof(char) * (strlen(opref)+14));
+    if(config.KeepCpG) {
+        sprintf(oname, "%s_CpG.bedGraph", opref);
+        config.output_fp[0] = fopen(oname, "w");
+        if(config.output_fp[0] == NULL) {
+            fprintf(stderr, "Couldn't open the output CpG metrics file for writing! Insufficient permissions?\n");
+            return -3;
+        }
+        fprintf(config.output_fp[0], "track type=\"bedGraph\" description=\"%s CpG methylation levels\"\n", opref);
+    }
+    if(config.KeepCHG) {
+        sprintf(oname, "%s_CHG.bedGraph", opref);
+        config.output_fp[1] = fopen(oname, "w");
+        if(config.output_fp[1] == NULL) {
+            fprintf(stderr, "Couldn't open the output CHG metrics file for writing! Insufficient permissions?\n");
+            return -3;
+        }
+        fprintf(config.output_fp[0], "track type=\"bedGraph\" description=\"%s CHG methylation levels\"\n", opref);
+    }
+    if(config.KeepCHH) {
+        sprintf(oname, "%s_CHH.bedGraph", opref);
+        config.output_fp[2] = fopen(oname, "w");
+        if(config.output_fp[2] == NULL) {
+            fprintf(stderr, "Couldn't open the output CHH metrics file for writing! Insufficient permissions?\n");
+            return -3;
+        }
+        fprintf(config.output_fp[0], "track type=\"bedGraph\" description=\"%s CHH methylation levels\"\n", opref);
+    }
+
+    //Run the pileup
+    extractCalls(&config);
+
+    //Close things up
+    hts_close(config.fp);
+    fai_destroy(config.fai);
+    if(config.KeepCpG) fclose(config.output_fp[0]);
+    if(config.KeepCHG) fclose(config.output_fp[1]);
+    if(config.KeepCHH) fclose(config.output_fp[2]);
+    hts_idx_destroy(config.bai);
+    free(opref);
+    free(oname);
+    free(config.output_fp);
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+PileOMeth (a temporary name derived due to it using a PILEup to extract METHylation metrics) will process a coordinate-sorted and indexed BAM file containing some form of BS-seq alignments and extract per-base methylation metrics from them. PileOMeth requires an indexed fasta file containing the reference genome as well.
+
+Prerequisites
+=============
+
+PileOMeth requires HTSlib version 1.1 or newer (version 1.0 is currently unsupported).
+
+Compilation
+===========
+
+PileOMeth can either dynamically link to HTSlib or statically incorporate it. To compile for dynamic linkage, use:
+
+`gcc -Wall -O3 -I/path/to/headers -L/path/to/libhts.so -o PileOMeth PileOMeth.c -lhts -lz -lpthread`
+
+If your HTSlib headers are under ~/include/htslib then use `-I~/include`.
+
+If you prefer to statically incorporate HTSlib (e.g., due to this being the only program needing it), compile HTSlib and then use the following:
+
+`gcc -Wall -O3 -I/path/to/htslib/compilation -o PileOMeth PileOMeth.c /path/to/htslib/compilation/libhts.a -lz -lpthread`
+
+If you downloaded and compiled HTSlib in ~/Downloads/htslib-1.1, then `/path/to/htslib/compilation` is `~/Downloads/htslib-1.1`.
+
+Usage
+=====
+
+The most basic usage of PileOMeth is as follows:
+
+`PileOMeth reference_genome.fa alignments.bam`
+
+This will calculate per-base CpG metrics and output them to `alignments_CpG.bedGraph`, which is a standard bedGraph file with column 4 being the number of reads/read pairs with evidence for a methylated C at a given position and column 5 the equivalent for an unmethylated C. An alternate output filename prefix can be specified with the `-o some_new_prefix` option.
+
+By default, PileOMeth will only calculate CpG metrics, but CHG and CHH metrics are supported as well (see the --CHH and --CHG options). If you would like to ignore CpG, metrics, simply specify --noCpG. Each type of metric is output to a different file.
+
+PileOMeth can filter reads and bases according to MAPQ and Phred score, respectively. The default minimums are MAPQ >= 10 and Phred >= 5, though these can be changed with the -q and -p options.
+
+To do list
+==========
+
+ * Add an -l option to parse only regions in a BED file
+ * add an -r/--region REGION option to just look at one region
+ * Enable ignoring discordant and/or singleton alignments
+ * Enable trimming based on M-bias
+ * Allow users to toggle the max depth (it's currently 2000)
+ * Support CRAM files
+ * Is it possible to support non-directional libraries with this method?
+ * If a fasta file isn't indexed, we should do that automatically
+ * If a BAM (or ideally CRAM) file isn't yet indexed, we should do that automatically
+ * Perhaps we should auto-ignore obvious multi-mappers (NH:i > 1) rather than rely on the MAPQ score
+ * Similarly, we should simply skip marked secondary alignments.
+ * What about marked duplicates? Perhaps make that up to the user with default=ignore.
+ * Is the output format the most convenient (it's what Bison uses, so converters have already been written)?
+ * Test to ensure that the results are correct!
