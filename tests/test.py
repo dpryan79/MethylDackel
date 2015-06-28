5bec67736be00c2b18184736b63b34a8d9972cbe
Björn Grüning <bjoern@gruenings.eu>
2015-06-28 13:45:31 +0200

    Update test cases.
    I updated the tests so they are able to be executed again. Unfortunately, the second test does not pass :(

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ from subprocess import check_call
 import os
 import os.path as op
 
+
 def rm(f):
     try:
         os.unlink(f)
@@ -9,25 +10,24 @@ def rm(f):
         pass
 
 rm('po')
-check_call('gcc ../PileOMeth.c ../bed.c -lhts -lz -o po', shell=True)
-assert op.exists('po')
+check_call('cd .. && make', shell=True)
+assert op.exists('../PileOMeth')
 
 rm('ct_aln_CpG.bedGraph')
-check_call('./po ct100.fa ct_aln.bam -q 2', shell=True)
+check_call('../PileOMeth extract ct100.fa ct_aln.bam -q 2', shell=True)
 assert op.exists('ct_aln_CpG.bedGraph')
 lines = sum(1 for _ in open('ct_aln_CpG.bedGraph'))
 assert lines == 1
 
 
-
 rm('cg_aln_CpG.bedGraph')
-check_call('./po cg100.fa cg_aln.bam -q 2', shell=True)
+check_call('../PileOMeth extract cg100.fa cg_aln.bam -q 2', shell=True)
 assert op.exists('cg_aln_CpG.bedGraph')
 lines = sum(1 for _ in open('cg_aln_CpG.bedGraph'))
 assert lines > 1
 
 # should be none with q > 10
-check_call('./po cg100.fa cg_aln.bam -q 10', shell=True)
+check_call('../PileOMeth extract cg100.fa cg_aln.bam -q 10', shell=True)
 assert op.exists('cg_aln_CpG.bedGraph')
 lines = sum(1 for _ in open('cg_aln_CpG.bedGraph'))
 assert lines == 1
